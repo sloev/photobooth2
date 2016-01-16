9e1bb7580a3b87ea9222ee30d9207d0b5a7e1c7c
sloev <johannes.valbjorn@gmail.com>
2016-01-16 22:55:13 +0100

    sdf

--- modules/printer.py
+++ modules/printer.py
@@ -3,10 +3,24 @@
 #                    to simple ESC-POS image on stdout
 # scruss - 2014-07-26 - WTFPL (srsly)
 
+import shutil
 import multiprocessing
 import sys
-from PIL import Image, ImageEnhance,ImageOps
+from PIL import Image, ImageOps
 import struct
+
+def compose_image(img):
+    #image = ImageOps.fit(img, (img.height, img.height))
+    bbox = img.getbbox()
+    image = img.crop(((bbox[2]/2)-(bbox[3]/2),0,(bbox[2]/2)+(bbox[3]/2),bbox[3]))
+    image = image.resize((384,384))
+    #image = ImageOps.expand(image, 1,(255,255,255))
+    Image = ImageOps.grayscale(image)
+    enh = ImageEnhance.Brightness(image)
+    image = enh.enhance(1.3)
+    enh = ImageEnhance.Contrast(image)
+    image = enh.enhance(1.3)
+    return image
 
 class PrinterWorker(multiprocessing.Process):
     def __init__(self, _queue):
@@ -21,27 +35,16 @@ class PrinterWorker(multiprocessing.Process):
         for filename in iter(self._queue.get, None):
             if filename:
                 image = Image.open(filename)
-                image = self._compose_image(image)
-                self._printer.print_image(image)
+                image = compose_image(image)
+                filename = filename + ".bin"
+                self._printer.print_image(image, filename)
                 counter += 1
                 if counter > 1:
                     counter = 0
-                    self._printer.print_image(self._strip)
+                    self._printer.print_image(self._strip, filename)
                     sys.stderr.write("send two images, and strip to the printer!\n")
         sys.stderr.write("printer joined\n")
 
-    def _compose_image(self, img):
-        #image = ImageOps.fit(img, (img.height, img.height))
-        bbox = img.getbbox()
-        image = img.crop(((bbox[2]/2)-(bbox[3]/2),0,(bbox[2]/2)+(bbox[3]/2),bbox[3]))
-        image = image.resize((384,384))
-        #image = ImageOps.expand(image, 1,(255,255,255))
-        image=ImageOps.grayscale(image)
-        enh = ImageEnhance.Brightness(image)
-        image = enh.enhance(1.3)
-        enh = ImageEnhance.Contrast(image)
-        image = enh.enhance(1.3)
-        return image
 
 class Printer():
     """Thermal printer
@@ -50,7 +53,7 @@ class Printer():
         pass
 
     # print all of the images!
-    def print_image(self, image):
+    def print_image(self, image, filename):
         if image.mode != '1':
             image = image.convert('1')
 
@@ -61,23 +64,29 @@ class Printer():
         image = image.convert('1')
 
         # output header (GS v 0 \000), width, height, image data
-        sys.stdout.write(''.join(('\x1d\x76\x30\x00',
+        with open(filename, "w") as f:
+            f.write(''.join(('\x1d\x76\x30\x00',
                                   struct.pack('2B', image.size[0] / 8 % 256,
                                               image.size[0] / 8 / 256),
                                               struct.pack('2B', image.size[1] % 256,
                                                           image.size[1] / 256),
                                                           image.tobytes())))
-        sys.stdout.flush()
+        try:
+            shutil.copyfile(filename, "/dev/usb/lp0")
+        except Exception:
+            pass
 def main():
     # give usage and exit if no arguments
     if len(sys.argv) == 1:
         print 'Usage:', sys.argv[0], \
                'image1 image2 ... [ > printer_device ]'
         exit(1)
-
-    image = Image.open(sys.argv[1])
+    filename = sys.argv[1]
+    image = Image.open(filename)
+    image = compose_image(image)
     printer = Printer()
-    printer.print_image(image)
+    filename = filename + ".bin"
+    printer.print_image(image, filename)
 
 if __name__ == "__main__":
     main()
