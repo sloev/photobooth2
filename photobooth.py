251243a9440507b1b3953ae07ee70b627fb7dc8f
Johannes Gårdsted Valbjørn <sloev@users.noreply.github.com>
2017-02-11 03:17:26 +0100

    Update photobooth.py

--- photobooth.py
+++ photobooth.py
@@ -25,7 +25,7 @@ def fade_led(wiringpi):
     for i in range(0,301,1):
         wiringpi.pwmWrite(18,i)
         time.sleep(0.004)
-    time.sleep(3)
+    time.sleep(4)
     for i in range(301,-1,-1):
         wiringpi.pwmWrite(18,i)
         time.sleep(0.003)
